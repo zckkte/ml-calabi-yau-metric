1ba48af26fa90facf2447c667113cbda364cc907
Zack Kite <kite.antonio@gmail.com>
2020-06-16 13:16:00 +0200

    fix namespacing

---
+++ donaldson_eval_helpers.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def hermeticity(a, rtol=1e-08, atol=1e-08):
+    return np.sum(np.isclose(a, np.conj(a.T), rtol=rtol, atol=atol))
+
+abs_error = lambda h_m, h_n : np.sum(np.abs(h_m - h_n  ))
+
+rel_error = lambda h_m, h_n : np.sum(np.abs(h_m-h_n)) / np.sum(np.abs(h_m))
