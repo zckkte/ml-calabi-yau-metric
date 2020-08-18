130c20b16bb2e6ad367ea24297afb15430d958d3
Zack Kite <kite.antonio@gmail.com>
2020-08-18 12:21:25 +0200

    fix to_hermitian; parallelise sigma_loss

--- cy_metric_dnn.py
+++ cy_metric_dnn.py
@@ -10,35 +10,33 @@ from donaldson import donaldson, initial_balanced_metric
 COORDINATES = 5
 LEARNING_RATE = 1e-4
 
-def main(epochs=3, batch_size=32, sample_size=20, no_of_samples = 10000):
+def main(epochs=3, batch_size=32, sample_size=28, no_of_samples = 10000):
     model = config_model()
-    model.compile(optimizer = keras.optimizers.Adam(learning_rate=LEARNING_RATE), metrics=['accuracy'])
+    model.compile(optimizer = keras.optimizers.Adagrad(learning_rate=LEARNING_RATE), metrics=['accuracy'])
 
     features = convert_to_ndarray(fq.quintic_point_weights(no_of_samples))
     train_dataset = tf.data.Dataset.from_tensor_slices((features, features))
     train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
-    model = model_train(model, train_dataset, sample_size, epochs)
+    model = model_train(model, train_dataset, batch_size, sample_size, epochs)
 
     model.save('%d_model_b%d_s%d_n%d.h5' % (int(time()), batch_size, sample_size, no_of_samples))
 
 convert_to_ndarray = (lambda point_weights : 
     np.array(list(map(lambda pw : np.append(pw['point'].view(np.float32), pw['weight']), point_weights)), dtype=np.float32))
 
-def model_train(model, train_dataset, sample_size=4, epochs=5):
+def model_train(model, train_dataset, batch_size, sample_size=4, epochs=5):
     for epoch in range(1, epochs + 1):
         print("epoch %d/%d" % (epoch, epochs))
         for step, (x_batch_train, _) in enumerate(train_dataset): 
-            losses = model_train_step(model, x_batch_train, sample_size)
+            losses = model_train_step(model, x_batch_train, sample_size, batch_size)
             if step % 50 == 0: 
-                #log every 50th batch loss
                 print('batch loss: %f, avg. loss: %f' % ( tf.reduce_sum(losses), tf.math.reduce_mean(losses)))
     return model
 
-@tf.function
-def model_train_step(model, x_batch_train, sample_size):
+def model_train_step(model, x_batch_train, sample_size, batch_size):
     with tf.GradientTape() as tape:
         logits = model(x_batch_train, training=True)
-        losses = sigma_loss(sample_size)(x_batch_train, logits)
+        losses = sigma_loss(sample_size, batch_size)(x_batch_train, logits)
     grads = tape.gradient(losses, model.trainable_weights)
     model.optimizer.apply_gradients(zip(grads, model.trainable_weights))
     return losses
@@ -53,7 +51,7 @@ def config_model():
     model.add(keras.layers.Dense((COORDINATES * 2) + 2))
     return model
 
-pad_input = lambda x : tf.map_fn(lambda y : pad_to_output_dim(y,(COORDINATES * 2) + 2), x)
+pad_input = lambda x : tf.vectorized_map(lambda y : pad_to_output_dim(y,(COORDINATES * 2) + 2), x)
 
 pad_to_output_dim = lambda t, out_dim : tf.pad(t, [[0, tf.abs(out_dim - t.shape[0]) ]], "CONSTANT")
 
@@ -68,44 +66,46 @@ def to_hermitian(x):
     real, imag = x[::2], x[1::2]
     values = tf.complex(real, imag) 
     dig_upper = fill_triangular(values)
-    lower_tri = tf.transpose(tf.linalg.band_part(dig_upper, 0, -1), conjugate=True) 
+    lower_tri =tf.math.conj(tf.transpose(tf.linalg.set_diag(dig_upper, tf.zeros(3, dtype=tf.complex64))))
     return dig_upper + lower_tri
 
-def sigma_loss(sample_size): 
+def sigma_loss(sample_size, batch_size): 
     sigma_error_vec = lambda pairs : tf.foldl (lambda sigma_acc, pair : sigma_acc 
-        + tf_metric_measures.sigma_error(pair), pairs, 0.)
-    sigma= lambda sample_pairs : tf.map_fn( lambda pairs : sigma_error_vec(pairs), sample_pairs, dtype=tf.float32)  
+        + tf_metric_measures.sigma_error(pair), pairs, 0., parallel_iterations=batch_size)
+    sigma = lambda sample_pairs : tf.map_fn(sigma_error_vec, sample_pairs, dtype=tf.float32, 
+        parallel_iterations=batch_size)
 
     #HACK: abusing y_true argument for the purpose of providing x_true as input to loss function
-    return lambda x_true, y_pred : \
+    return tf.function(lambda x_true, y_pred : \
         sigma(sample_tuples(concat_point_weight_det(x_true[:, 0:2 * COORDINATES + 1], \
-            extract_y_pred_to_hermitian(y_pred)), sample_size))
+            extract_y_pred_to_hermitian(y_pred, batch_size )), sample_size, batch_size )) )
 
 #HACK: keras does not allow for y_true and y_pred to be of differring shapes 
-extract_y_pred_to_hermitian = lambda y_pred: tf.map_fn(lambda y : to_hermitian(y), y_pred, dtype=tf.complex64)
+extract_y_pred_to_hermitian = tf.function(lambda y_pred, batch_size : tf.vectorized_map(to_hermitian, y_pred))
 
 def to_complex_point_weight(x_true):
     point_real = x_true[:, 0:COORDINATES * 2][:, ::2]
     point_imag = x_true[:, 0:COORDINATES * 2][:, 1::2]
     point_complex = tf.complex(point_real, point_imag)
     weights = x_true[:, -1]
-
     return tf.concat([point_complex, cast_expand_dim (weights)], axis=1)
 
+@tf.function
 def concat_point_weight_det(x_true, metrics):
     determinants = tf.linalg.det(metrics)
     return tf.concat([ to_complex_point_weight(x_true), cast_expand_dim(determinants) ], axis=1)
 
-cast_expand_dim = lambda arr : tf.cast(tf.expand_dims(arr, 1), dtype=tf.complex64)
+cast_expand_dim = lambda arr : tf.cast(tf.expand_dims(arr, 1), dtype=tf.complex64) 
 
-def sample_tuples(metric_point_weights, sample_size):
-    exclude_mask = (lambda x, arr : tf.map_fn(lambda p : tf.reduce_all( tf.equal(p, x) ) == False,  
-        arr, dtype=tf.bool))
+@tf.function
+def sample_tuples(metric_point_weights, sample_size, batch_size):
+    exclude_mask = lambda x, arr : tf.vectorized_map(lambda p : tf.equal(tf.reduce_all(tf.equal(p, x)), False), arr)
     exclude = lambda x, arr : tf.boolean_mask(arr, exclude_mask(x, arr))
-    sample_with_exclude = lambda x, arr : random_choice(exclude(x, arr), sample_size, axis=0) 
-    stack_map = lambda x, arr : tf.map_fn(lambda y : tf.stack([x, y]), arr)
 
-    return tf.map_fn(lambda x : stack_map(x, sample_with_exclude(x, metric_point_weights)), metric_point_weights)
+    sample_with_exclude = lambda x, arr : random_choice(exclude(x, arr), sample_size, axis=0)
+    stack_map = lambda x, arr : tf.vectorized_map(lambda y : tf.stack([x, y]), arr)
+    return tf.map_fn(lambda x : stack_map(x, sample_with_exclude(x, metric_point_weights)), metric_point_weights,
+        parallel_iterations=batch_size)
 
 def random_choice(x, size, axis=0, unique=True):
     dim_x = tf.cast(tf.shape(x)[axis], tf.int64)
--- tf_metric_measures.py
+++ tf_metric_measures.py
@@ -1,10 +1,9 @@
-from joblib import Parallel, delayed
-from functools import * 
 import tensorflow as tf
 
 COORDINATES = 5
 PURELY_REAL = tf.cast(tf.complex(1.,0.), dtype=tf.complex64)
 
+@tf.function
 def sigma_error(g_point_weights):
     """ tensorflow implementation of sigma error 
         g_point_weights = [ [...point, ...weight, ...determinant ], ...]
@@ -16,10 +15,10 @@ def sigma_error(g_point_weights):
 
     return ((n_t * vol_cy) ** (-1) * tf.foldl(lambda acc, gpw : acc 
             + tf.abs(1 - __determinant(gpw) * vol_cy / (omega_wedge_omega_conj(__point(gpw)) * vol_k )) * __weight(gpw), 
-            g_point_weights, 0.))
+            g_point_weights, 0., parallel_iterations=n_t))
 
 volume_k = lambda n_t, g_point_weights : ((1 / n_t) * tf.foldl(lambda acc, gpw : acc 
-    + vol_k_integrand(gpw), g_point_weights, 0.) )
+    + vol_k_integrand(gpw), g_point_weights, 0., parallel_iterations=n_t) )
 
 elim_z_j = lambda z : (-1) - tf.reduce_sum(z[good_coord_mask(z)] ** 5)
 
@@ -28,8 +27,7 @@ good_coord_mask = lambda x : (x != x[find_max_dq_coord_index(x)]) & exclude_affi
 exclude_affine_max = lambda z_j : tf.cond(isclose(z_j, PURELY_REAL) == False, \
     lambda : tf.abs(tf.math.pow(z_j, 4)), lambda: 0. )
 
-find_max_dq_coord_index = lambda z : \
-    tf.argmax(tf.map_fn(lambda z_j : exclude_affine_max(z_j), z, dtype=tf.float32)) 
+find_max_dq_coord_index = lambda z : tf.argmax(tf.map_fn(exclude_affine_max, z, dtype=tf.float32)) 
 
 isclose = lambda a, b, rtol=1.e-5, atol=1.e-8 : tf.math.less_equal(tf.abs(a - b), atol + rtol * tf.abs(b)) 
 
