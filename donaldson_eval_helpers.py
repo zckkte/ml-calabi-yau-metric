36afc43ed253762c0ff49c02eff539349862bc4a
Zack Kite <zackkite@student-228-86.eduroam.uu.se>
2020-09-07 16:44:51 +0200

    add donaldson relative error plot

--- donaldson_eval_helpers.py
+++ donaldson_eval_helpers.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def hermeticity(a, rtol=1e-08, atol=1e-08):
+def hermitian_elements(a, rtol=1e-08, atol=1e-08):
     return np.sum(np.isclose(a, np.conj(a.T), rtol=rtol, atol=atol))
 
 abs_error = lambda h_m, h_n : np.sum(np.abs(h_m - h_n  ))
